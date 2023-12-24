6c1250bede66fa16dab1c0f9f74b72d3a651cd82
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-12-24 20:39:13 +0100

    Succesfully registered a bell

--- src/main/java/net/callbells/commands/CommandBellRegister.java
+++ src/main/java/net/callbells/commands/CommandBellRegister.java
@@ -1,10 +1,15 @@
 package net.callbells.commands;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import net.callbells.CallBells;
+import net.callbells.util.RegistryUtil;
 
 public class CommandBellRegister extends Command {
 
@@ -22,7 +27,44 @@ public class CommandBellRegister extends Command {
             return true;
         }
 
+        // Check if the player is looking at a bell
+        if (player.getTargetBlockExact(5) == null || player.getTargetBlockExact(5).getType() != Material.BELL) {
+            player.sendMessage("You must be looking at a bell to register it!");
+            return true;
+        }
+
+        // Get the bell's location as a string
+        String bellLocation = player.getTargetBlockExact(5).getLocation().toString();
+
+        // Check if the bell is already registered
+        if (RegistryUtil.isBellRegistered(bellLocation)) {
+            // Bell is already registered
+            // Add player's UUID to the bell if not already added
+            List<UUID> bellOwners = RegistryUtil.getBellOwners(bellLocation);
+            UUID playerUUID = player.getUniqueId();
+
+            if (!bellOwners.contains(playerUUID)) {
+                bellOwners.add(playerUUID);
+                RegistryUtil.updateBellOwners(bellLocation, bellOwners);
+                player.sendMessage("You've been added as an owner to the registered bell!");
+            } else {
+                player.sendMessage("You are already an owner of this bell!");
+            }
+        } else {
+            // Bell is not registered
+            // Check if a name is provided in the command
+            if (args.length < 2) {
+                player.sendMessage("Please provide a name for the bell: /registerbell <name>");
+                return true;
+            }
+
+            String bellName = args[1];
+
+            // Register the bell with its name, location, and the player's UUID
+            RegistryUtil.registerBell(bellLocation, bellName, player.getUniqueId());
+            player.sendMessage("Bell '" + bellName + "' registered!");
+        }
+
         return true;
     }
 }
-
--- src/main/java/net/callbells/util/RegistryUtil.java
+++ src/main/java/net/callbells/util/RegistryUtil.java
@@ -6,7 +6,9 @@ import net.callbells.CallBells;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class RegistryUtil {
 
@@ -18,7 +20,7 @@ public class RegistryUtil {
         config = YamlConfiguration.loadConfiguration(configFile);
 
         if (!configFile.exists()) {
-            CallBells.getInstance().saveResource("bells.yml", false);
+            saveDefaultResource("bells.yml");
         }
     }
 
@@ -28,16 +30,54 @@ public class RegistryUtil {
     }
 
     // Example method to register a bell location
-    public static void registerBell(String bellLocation) {
+    public static void registerBell(String bellLocation, String bellName, UUID ownerUUID) {
         List<String> registeredBells = config.getStringList("registered-bells");
         registeredBells.add(bellLocation);
         config.set("registered-bells", registeredBells);
 
+        // Store the owner UUID for the bell
+        String ownersPath = "bell-owners." + bellLocation;
+        List<UUID> owners = getBellOwners(bellLocation);
+        owners.add(ownerUUID);
+        config.set(ownersPath, owners.stream().map(UUID::toString).toList());
+
+        // Store the bell name
+        String namesPath = "bell-names." + bellLocation;
+        config.set(namesPath, bellName);
+
+        saveConfig();
+    }
+
+    // Check if a bell is already registered
+    public static boolean isBellRegistered(String bellLocation) {
+        List<String> registeredBells = config.getStringList("registered-bells");
+        return registeredBells.contains(bellLocation);
+    }
+
+    // Get the owners of a bell
+    public static List<UUID> getBellOwners(String bellLocation) {
+        String ownersPath = "bell-owners." + bellLocation;
+        return config.contains(ownersPath) ? config.getStringList(ownersPath).stream().map(UUID::fromString).toList() : new ArrayList<>();
+    }
+
+    // Update the owners of a bell
+    public static void updateBellOwners(String bellLocation, List<UUID> owners) {
+        String ownersPath = "bell-owners." + bellLocation;
+        config.set(ownersPath, owners.stream().map(UUID::toString).toList());
         saveConfig();
     }
 
     // Save the configuration to the file
     private static void saveConfig() {
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Save the default resource to the plugin's data folder if it doesn't exist
+    private static void saveDefaultResource(String resource) {
         try {
             config.save(configFile);
         } catch (IOException e) {
